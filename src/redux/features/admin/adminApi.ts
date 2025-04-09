9913fe44a05441d2269d89111af382497ad203e4
Raihan Sharif <raihan@Raihans-MacBook-Pro.local>
2025-04-10 05:45:57 +0600

    Create item added

--- src/redux/features/admin/adminApi.ts
+++ src/redux/features/admin/adminApi.ts
@@ -1,22 +1,31 @@
 import { baseApi } from "@/redux/api/baseApi";
 
 const adminApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        createAdmin: builder.mutation({
-            query: (data) => ({
-                url: 'users/create-admin',
-                method: 'POST',
-                body: data,
-                })
-            }),
-            getAdmin: builder.query({
-                query: () => ({
-                    url: 'admins',
-                    method: 'GET'
-                })
-            })
-}),
-        
-})
+  endpoints: (builder) => ({
+    createAdmin: builder.mutation({
+      query: (data) => ({
+        url: "users/create-admin",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    getAdmin: builder.query({
+      query: () => ({
+        url: "admins",
+        method: "GET",
+      }),
+    }),
+    getSingleAdmin: builder.query({
+      query: (userID) => ({
+        url: `admins/?id=${userID}`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
 
-export const {useCreateAdminMutation, useGetAdminQuery} = adminApi
+export const {
+  useCreateAdminMutation,
+  useGetAdminQuery,
+  useGetSingleAdminQuery,
+} = adminApi;
